d4938ba528ce19245cd79f53db6f4eb4d1ce0b77
Rahatutzaman Rizon <115326466+rahatutzaman-rizon@users.noreply.github.com>
2024-12-06 08:51:53 +0600

    Add30

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,28 @@
+import defaultTheme from 'daisyui/src/theming/themes/light.js';
+
 /** @type {import('tailwindcss').Config} */ 
 export default {
-  // Specify which files should be processed by Tailwind
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  
-  // DaisyUI theme configuration
-  daisyui: {
-    themes: [
-      {
-        light: {
-          // Extend the default light theme
-      
-          // Custom primary color
-          primary: "#27AAE1",
-          // Custom secondary color
-          secondary: "#091242",
-        },
-      },
-    ],
-  },
-  
-  // Extended Tailwind theme configuration
-  theme: {
-    extend: {
-      // Center the container by default
-      container: {
-        center: true,
-      },
-    },
-  },
-  
-  // Include DaisyUI plugin
-  plugins: [require("daisyui")],
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   
+   daisyui: {
+     themes: [
+       {
+         light: {
+           ...defaultTheme,
+           primary: "#27AAE1",
+           secondary: "#091242",
+         },
+       },
+     ],
+   },
+   
+   theme: {
+     extend: {
+       container: {
+         center: true,
+       },
+     },
+   },
+   
+   plugins: [import('daisyui')],
 };
